0a53d9af837397164ca77e90349a498550b33dec
luiz0777 <samuka.luiz07@gmail.com>
2025-04-30 20:32:25 -0300

    Add files via upload

---
+++ ex1.cpp
@@ -0,0 +1,24 @@
+  #include <iostream>
+  using namespace std;
+  
+  int main()
+  
+  {
+	 int b;
+	 int a;
+	 int area;
+	 
+	 cout << "informe a base do triangulo: ";
+	 cin >> b;
+	 cout << "informe a altura do triangulo: ";
+	 cin >> a;
+	 
+     area = (b * a);
+     
+	 cout << "a area do triangulo: ";
+	 cout << area;
+	 
+	
+	 
+	  return 0;
+  }
