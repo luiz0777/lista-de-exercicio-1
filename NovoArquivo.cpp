4f4068b36faedb5ef164a52617eef5f308459c53
luiz0777 <samuka.luiz07@gmail.com>
2025-04-30 20:07:56 -0300

    Add files via upload

---
+++ NovoArquivo.cpp
@@ -0,0 +1,20 @@
+  #include <iostream>
+  
+  using namespace std;
+  
+  int main()
+  
+  {
+  	
+  	double b;
+  	double a;
+  	double area;
+  	cout << "informe o valor da base do triangulo: ";
+  	cin >> b;
+  	cout << " informe o valor da altura do triangulo: ";
+  	cin >> a;
+  	area = (b * a) / 2;
+  	cout << " a area calculada do triangulo foi " << area;
+  	
+  	return 0;
+  }
